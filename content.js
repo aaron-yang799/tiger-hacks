5ac453d209aaa46c0e8743d1ba78c56b5958d94e
aaron-yang799 <aaron.yang799@gmail.com>
2023-11-04 14:59:04 -0500

    SUM

--- content.js
+++ content.js
@@ -1,16 +1,19 @@
-// content.js - This script is injected into the webpage
+console.log("CHORMY");
 
-// Function to send the data to the extension's background script or popup script
-function sendTextToPopup(text) {
-    chrome.runtime.sendMessage({ action: "grabbedText", text: text });
-  }
-  
-  // Listen for a message from the popup script to start the text grab when the user clicks the button
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === "grabText") {
-        const allText = document.body.innerText;
-        sendTextToPopup(allText); // Send the text back to the popup
-        sendResponse({ status: "Text grabbed" });
-    }
+
+const para = [];
+
+let paragraphs = document.getElementsByTagName('p');
+for(elt of paragraphs)
+{
+    paragraphs.push(elt);
+}
+
+paragraphs.forEach(entry)  
+{
+    console.log(entry);
 });
+
+
+
   
--- test.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.getSelected(null, function(tab){
-    chrome.tabs.sendRequest(tab.id, {method: "getText"}, function(response){
-        if(response.method=="getText"){
-            alltext = response.data;
-        }
-    });
-}); 
-
-console.log(alltext);
