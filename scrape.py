791ed95bbbf0d0137843e65ebd44c944f58da7fd
zachrsnyder <zachrsnyder2004@gmail.com>
2023-11-03 21:42:36 -0500

    testing

--- scrape.py
+++ scrape.py
@@ -3,5 +3,9 @@ from bs4 import BeautifulSoup
 
 url = "https://www.buzzfeed.com/mychalthompson/travis-kelce-taylor-swift-love-question-reactions?origin=spl"
 response = requests.get(url)
+html_content = response.text
+soup = BeautifulSoup(html_content, 'html.parser') 
+print(soup.decode)
 
 
+print(html_content)
