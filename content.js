0d56ab65d34b2e53464d38fb737daedd78a635f4
aaron-yang799 <aaron.yang799@gmail.com>
2023-11-04 15:23:40 -0500

    kjd

--- content.js
+++ content.js
@@ -1,19 +1,13 @@
 console.log("CHORMY");
 
-
 const para = [];
 
 let paragraphs = document.getElementsByTagName('p');
 for(elt of paragraphs)
 {
-    paragraphs.push(elt);
+    para.push(elt);
 }
 
-paragraphs.forEach(entry)  
-{
-    console.log(entry);
-});
-
-
-
-  
+for (let i = 0; i < para.length; i++) {
+    console.log(para[i]);
+}
