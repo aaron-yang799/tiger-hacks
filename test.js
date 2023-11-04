fe3c4e36e13c7eef3a0270ac4c7339a08836d485
aaron-yang799 <aaron.yang799@gmail.com>
2023-11-04 13:20:49 -0500

    askjdl

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
+function getText(){
+    return document.body. innerText
+}
+function getHTML(){
+    return document.body.outerHTML
+}
 
+console.log (getText());
+console.log(getHTML());
+//Gives you all the text on the page
+//Gives you the whole HTML of the page
