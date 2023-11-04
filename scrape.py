3f1aef22f184adeb3a89569e7670d35e26a051c8
zachrsnyder <zachrsnyder2004@gmail.com>
2023-11-04 12:27:05 -0500

    jhvjh

--- scrape.py
+++ scrape.py
@@ -1,23 +1,13 @@
 import requests
-import csv
 from bs4 import BeautifulSoup
+import csv
 
 
 url = "https://www.buzzfeed.com/mychalthompson/travis-kelce-taylor-swift-love-question-reactions?origin=spl"
 response = requests.get(url)
 html_content = response.text
 soup = BeautifulSoup(html_content, 'html.parser') 
-print(soup.decode)
+print(soup)
 
-lines = html_content.split('\n')
 
-# Now we will open a file to write into
-with open('output.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-
-    # Iterate over the lines and write to the csv file
-    for line in lines:
-        # Split each line into fields using the comma as a delimiter
-        writer.writerow(line.split(','))
-
-print(html_content)
+#print(html_content)
