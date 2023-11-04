4c0f382f0a09217f43ca31faa1ed1f9e324d84a4
aaron-yang799 <aaron.yang799@gmail.com>
2023-11-03 22:12:46 -0500

    meta

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 url = "https://www.buzzfeed.com/mychalthompson/travis-kelce-taylor-swift-love-question-reactions?origin=spl"
 response = requests.get(url)
 html_content = response.text
