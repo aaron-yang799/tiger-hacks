ee48d237c386b556e3ffa609588dd30b4d9efabc
aaron-yang799 <aaron.yang799@gmail.com>
2023-11-04 14:30:28 -0500

    kuahs

--- popup.js
+++ popup.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function() {
     var grabButton = document.getElementById('grab');
     grabButton.addEventListener('click', function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {action: "grabText"}, function(response) {
-          // You can now do something with the response if you want
-            console.log(response.status);
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            function: grabPageText
             });
         });
     });
@@ -17,4 +16,6 @@ function grabPageText() {
     console.log(text);
 // Do something with the page text here, like sending it to your extension's popup or background script
 }
+
+
   
