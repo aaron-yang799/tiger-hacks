41babfc8acc57e85ab2e25cac90eae5fc3d04637
aaron-yang799 <aaron.yang799@gmail.com>
2023-11-04 19:29:14 -0500

    hello

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 async function query(data) {
 	const response = await fetch(
-		"https://api-inference.huggingface.co/models/mediabiasgroup/babe-v3-roberta-fully-trained",
+		"https://api-inference.huggingface.co/models/Social-Media-Fairness/Classifier-Bias-SG",
 		{
 			headers: { Authorization: "Bearer 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" },
 			method: "POST",
@@ -11,6 +11,6 @@ async function query(data) {
 	return result;
 }
 
-query({"inputs": "Opinion Today — In an extravagant display of environmental tokenism, the City Council has decided to throw taxpayer money at a so-called Green Lungs project, planting 2,000 trees in urban parks that few asked for and fewer still will use. Defying common sense in a time when budget deficits are ballooning, the Council, in what some see as a stunt to polish its green credentials, has chosen to prioritize leaves over law enforcement. As crime statistics take a backseat, one wonders if future muggers will hide behind the council's expensive foliage."}).then((response) => {
+query({"inputs": "George Washington is the first president of the united states"}).then((response) => {
 	console.log(JSON.stringify(response));
 });
