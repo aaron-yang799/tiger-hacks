86c8192456b370beff29fb3953bfd9b681b4985f
Hocus Pocus <87617260+chesain@users.noreply.github.com>
2023-11-05 01:07:12 -0600

    test

--- d_model.py
+++ d_model.py
@@ -3,3 +3,6 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 tokenizer = AutoTokenizer.from_pretrained("valurank/distilbert-allsides")
 model = AutoModelForSequenceClassification.from_pretrained("valurank/distilbert-allsides")
+
+model.save_pretrained("./model")
+tokenizer.save_pretrained("./model")
