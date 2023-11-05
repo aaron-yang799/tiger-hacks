c87e189f62a4de61750e03d026e8bc9bf48cdd0c
aaron-yang799 <aaron.yang799@gmail.com>
2023-11-05 01:13:12 -0600

    hello

--- content.js
+++ content.js
@@ -1,3 +1,31 @@
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/valurank/distilbert-allsides",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
+
+function extractNumbers(obj, numbers = []) {
+	// Traverse all the object's properties
+	for (let key in obj) {
+	  if (typeof obj[key] === 'number') {
+		// If the property is a number, push it to the numbers array
+		numbers.push(obj[key]);
+	  } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+		// If the property is an object, recurse into it
+		extractNumbers(obj[key], numbers);
+	  }
+	}
+	return numbers;
+  }
+
+
 const para = [];
 
 let paragraphs = document.getElementsByTagName('p');
@@ -5,6 +33,21 @@ for(elt of paragraphs)
 {
     para.push(elt);
 }
+
+let test = para.join(". ");
+
+query({"inputs": test}).then((response) => {
+	console.log(response);
+	const sortOrder = { 'left': 1, 'center': 2, 'right': 3 };
+
+	response[0].sort((a, b) => sortOrder[a.label] - sortOrder[b.label]);
+
+	//console.log(JSON.stringify(response));
+	response = JSON.stringify(response);
+	var jsonObject = JSON.parse(response);
+	var numbersArray = extractNumbers(jsonObject); // Get all numbers in an array
+	console.log(numbersArray); //prints array
+});
 
 const boxIds = [
     "SumBox1", "SumBox2", "SumBox3", "SumBox4", "SumBox5",
