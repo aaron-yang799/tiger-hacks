8940e7d3e5b5022d1455b28ed25b7d6d7cdb6669
aaron-yang799 <aaron.yang799@gmail.com>
2023-11-05 04:31:44 -0600

    hello

--- content.js
+++ content.js
@@ -59,107 +59,106 @@ query({"inputs": queryString}).then((response) => {
     console.log(left);
     console.log(center);
     console.log(right);
-});
 
-const boxIds = [
-    "SumBoxR1", "SumBoxR2", "SumBoxR3", "SumBoxR4", "SumBoxR5",
-    "SumBoxC1", "SumBoxC2", "SumBoxC3", "SumBoxC4", "SumBoxC5",
-    "SumBoxL1", "SumBoxL2", "SumBoxL3", "SumBoxL4", "SumBoxL5",
-    "OppBoxR1", "OppBoxR2", "OppBoxR3", "OppBoxR4", "OppBoxR5",
-    "OppBoxC1", "OppBoxC2", "OppBoxC3", "OppBoxC4", "OppBoxC5",
-    "OppBoxL1", "OppBoxL2", "OppBoxL3", "OppBoxL4", "OppBoxL5"
-];
-
-const infoIds = [
-    "SumInfo", "OppInfo", "CentBox"
-];
-
-const data = {
-    labels: ['Right Lean', 'Central', 'Left Lean'],
-    datasets: [{
-        data: [30, 30, 40], // Values for each slice
-        backgroundColor: ['red', 'grey', 'blue'], // Colors for each slice
-    }],
-};
-
-for (let i = 0; i < para.length; i++) {
-    console.log(para[i]);
-}
-
-function getBias(){
-    return 1
-}
-
-function decideBox(){
-    const elements = document.querySelectorAll('.mini-box')
-    var i = 0;
-    elements.forEach(function(element) {
-        const elementName = boxIds[i];
-        const bias_rating = getBias();
-        const elemIndMatch = elementName.match(/\d/);
-        if (elemIndMatch) {
-            const elemInd = parseInt(elemIndMatch[0], 10); // Extract and convert to an integer
-            console.log(elemInd);
-            if(elemInd > bias_rating)
-            {
-                element.style.backgroundColor = 'rgb(235, 235, 235)'; 
-            }else{
-                element.style.backgroundColor = 'green';
-            };
-        }
-        i = i + 1;
-    });
-}
-
-function displayBias(){
-    var contentToWrite = "This is the content to be added to the paragraph.";
-    const elements = document.querySelectorAll('p');
-    var i = 0;
-    // Find the paragraph element by its ID
-    elements.forEach(function(element) {
-        const elementName = infoIds[i];
-        var paragraphElement = document.getElementById(elementName);
-
-        // Create a text node with your content
-        var textNode = document.createTextNode(contentToWrite);
-
-        // Append the text node to the paragraph element
-        paragraphElement.appendChild(textNode);
-        i = i + 1;
-    });
-}
-
-//This is the utilization of chatgpt API to generate a summary of the webpage content
-const content = "I need a summary of this message."
-const apiKey = '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';
-const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
-
-var helpme = `Summarize the content: ${content}`;
-console.log(helpme);
-//this fetch call uses the url and key to gain access to gpts API. Then a prompt is created that requests a summary of the provided content of max character size 150.
-fetch(apiUrl, {
-    method: "POST", //means we are sending data to GPT
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer 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`,
-    },
-    body: JSON.stringify({ 
-        prompt: `Summarize the content: ${content}`,
-        max_tokens: 150,
-    }),
-})
-    .then((response) => response.json())
-    .then((data2) => {
-        const summary = data2.choices[0].text;
-        console.log(summary);
+    const boxIds = [
+        "SumBoxR1", "SumBoxR2", "SumBoxR3", "SumBoxR4", "SumBoxR5",
+        "SumBoxC1", "SumBoxC2", "SumBoxC3", "SumBoxC4", "SumBoxC5",
+        "SumBoxL1", "SumBoxL2", "SumBoxL3", "SumBoxL4", "SumBoxL5",
+        "OppBoxR1", "OppBoxR2", "OppBoxR3", "OppBoxR4", "OppBoxR5",
+        "OppBoxC1", "OppBoxC2", "OppBoxC3", "OppBoxC4", "OppBoxC5",
+        "OppBoxL1", "OppBoxL2", "OppBoxL3", "OppBoxL4", "OppBoxL5"
+    ];
+    
+    const infoIds = [
+        "SumInfo", "OppInfo", "CentBox"
+    ];
+    
+    const data = {
+        labels: ['Right Lean', 'Central', 'Left Lean'],
+        datasets: [{
+            data: [30, 30, 40], // Values for each slice
+            backgroundColor: ['red', 'grey', 'blue'], // Colors for each slice
+        }],
+    };
+    
+    for (let i = 0; i < para.length; i++) {
+        console.log(para[i]);
+    }
+    
+    function getBias(){
+        return 1
+    }
+    
+    function decideBox(){
+        const elements = document.querySelectorAll('.mini-box')
+        var i = 0;
+        elements.forEach(function(element) {
+            const elementName = boxIds[i];
+            const bias_rating = getBias();
+            const elemIndMatch = elementName.match(/\d/);
+            if (elemIndMatch) {
+                const elemInd = parseInt(elemIndMatch[0], 10); // Extract and convert to an integer
+                console.log(elemInd);
+                if(elemInd > bias_rating)
+                {
+                    element.style.backgroundColor = 'rgb(235, 235, 235)'; 
+                }else{
+                    element.style.backgroundColor = 'green';
+                };
+            }
+            i = i + 1;
+        });
+    }
+    
+    function displayBias(){
+        var contentToWrite = "This is the content to be added to the paragraph.";
+        const elements = document.querySelectorAll('p');
+        var i = 0;
+        // Find the paragraph element by its ID
+        elements.forEach(function(element) {
+            const elementName = infoIds[i];
+            var paragraphElement = document.getElementById(elementName);
+    
+            // Create a text node with your content
+            var textNode = document.createTextNode(contentToWrite);
+    
+            // Append the text node to the paragraph element
+            paragraphElement.appendChild(textNode);
+            i = i + 1;
+        });
+    }
+    
+    //This is the utilization of chatgpt API to generate a summary of the webpage content
+    const content = "I need a summary of this message."
+    const apiKey = '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';
+    const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
+    
+    var helpme = `Summarize the content: ${content}`;
+    console.log(helpme);
+    //this fetch call uses the url and key to gain access to gpts API. Then a prompt is created that requests a summary of the provided content of max character size 150.
+    fetch(apiUrl, {
+        method: "POST", //means we are sending data to GPT
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer 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`,
+        },
+        body: JSON.stringify({ 
+            prompt: `Summarize the content: ${content}`,
+            max_tokens: 150,
+        }),
     })
-    .catch((error) => {
-        console.error("Error:", error);
-    });
-
-window.onload = function() {
-    decideBox();
-    //decidePie();
-    displayBias();
-};
-
+        .then((response) => response.json())
+        .then((data2) => {
+            const summary = data2.choices[0].text;
+            console.log(summary);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    
+    window.onload = function() {
+        decideBox();
+        //decidePie();
+        displayBias();
+    };
+});
