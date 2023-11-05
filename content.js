3e56ab74f82fbcefb57fa64e482abc84d98bcc9c
zachrsnyder <zachrsnyder2004@gmail.com>
2023-11-04 20:05:17 -0500

    ebn

--- content.js
+++ content.js
@@ -8,13 +8,46 @@ for(elt of paragraphs)
     para.push(elt);
 }
 
+const boxIds = [
+    "SumBox1", "SumBox2", "SumBox3", "SumBox4", "SumBox5",
+    "OppBox1", "OppBox2", "OppBox3", "OppBox4", "OppBox5",
+    "CentBox1", "CentBox2", "CentBox3", "CentBox4", "CentBox5"
+];
+
 for (let i = 0; i < para.length; i++) {
     console.log(para[i]);
 }
 
-function decideBox(index, bias_rating){
-    if(index > bias_rating)
-    {
-        
-    }
+function getBias(){
+    return 2
+}
+
+function decideBox(){
+    const elements = document.querySelectorAll('.mini-box')
+    var i = 0;
+    elements.forEach(function(element) {
+        const elementName = boxIds[i];
+        console.log(elementName);
+        const bias_rating = getBias();
+        const elemIndMatch = elementName.match(/\d/);
+        if (elemIndMatch) {
+            const elemInd = parseInt(elemIndMatch[0], 10); // Extract and convert to an integer
+            console.log(elemInd);
+            if(elemInd > bias_rating)
+            {
+                element.style.backgroundColor = 'rgb(235, 235, 235)'; 
+            }else{
+                element.style.backgroundColor = 'green';
+            };
+        }else console.log("shit");
+        i = i + 1;
+    });
+}
+
+function allBox(){
+    const elements = document.querySelectorAll('.mini-box')
+    elements.forEach(function(element) {
+        element.style.backgroundColor = 'green';
+        console.log('fuck')
+    })
 }
